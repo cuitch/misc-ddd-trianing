8d0db0fecfa31a36e44e4de7e22dbd2efc2a6cb3
Marvin Cheng <cuit_chenghuan@163.com>
2023-02-22 12:55:41 +0800

    'refactor'

--- sys-setting/src/test/java/com/ch/test/SysSettingApplicationTest.java
+++ sys-setting/src/test/java/com/ch/test/SysSettingApplicationTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.UUID;
+
 @Slf4j
 @SpringBootTest
 public class SysSettingApplicationTest {
@@ -16,6 +18,13 @@ public class SysSettingApplicationTest {
     @Test
     public void insertPost(){
         PostPO postPO = new PostPO();
+        postPO.setSeq(1);
+        postPO.setCreateUserId(1L);
+        postPO.setUpdateUserId(1L);
+        postPO.setCreatedTime(System.currentTimeMillis());
+        postPO.setUpdatedTime(System.currentTimeMillis());
+        postPO.setGroupId(1L);
+        postPO.setUuid(UUID.randomUUID().toString().replace("-",""));
         postPO.setName("test1234567890");
 
         postMapper.insert(postPO);
